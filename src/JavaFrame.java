ebca8d11c4cd0cae08619c53c5810f0d7e51a596
Kodiererin <iamujjwal45@gmail.com>
2023-04-24 00:22:56 +0530

    Adding Files

--- src/JavaFrame.java
+++ src/JavaFrame.java
@@ -1,5 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 
@@ -16,6 +19,7 @@ public class JavaFrame extends JFrame {
 	javax.swing.JButton nine;
 	
 	
+	
 	JavaFrame(){
 		zero = new javax.swing.JButton("0");
 		one = new javax.swing.JButton("1");
@@ -28,19 +32,21 @@ public class JavaFrame extends JFrame {
 		eight = new javax.swing.JButton("8");
 		nine = new javax.swing.JButton("9");
 		
-		setLayout(new BorderLayout());
-		add(zero , RIGHT_ALIGNMENT);
-		add(one , RIGHT_ALIGNMENT);
-		add(two);
-		add(three);
-		add(four);
-		add(five);
-		add(six);
-		add(seven);
-		add(eight);
-		add(nine);
+		setLayout(new BorderLayout(20,15));
+		
+		add(zero,BorderLayout.WEST);
+		add(one,BorderLayout.CENTER);
+		add(two,BorderLayout.CENTER);
+		add(three,BorderLayout.CENTER);
+		add(four,BorderLayout.CENTER);
+		add(five,BorderLayout.CENTER);
 		
 	}
-	
+	public static void main(String[] args) {
+		JavaFrame frame = new JavaFrame();
+		frame.setSize(400,700);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	
 }
